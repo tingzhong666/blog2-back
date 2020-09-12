f731eb4fef6ea8a44d4a09976b6ea080e3ac52c9
tingzhong666 <tingzhong666@163.com>
2020-09-12 12:07:54 +0800

    .

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 const router = require('express').Router(),
   check = require('../middlewares/check')
 
-router.post('/auth', check)
+router.post('/auth', check, async (req, res) => {
+  res.send({
+    code: 1,
+    data: {}
+  })
+})
 
 module.exports = router
